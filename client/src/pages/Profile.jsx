461bb15f1576158deabf1c6c19de80d4e9ca1028
Ayan Mondal <ayan.official.mail.id@gmail.com>
2024-08-23 04:26:29 +0530

    create listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,6 +20,7 @@ import {
 } from '../redux/user/userSlice';
 
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 
@@ -191,6 +192,12 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'} 
         </button>
+        <Link
+          className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
 
       </form>
 
